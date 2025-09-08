ddd378cf95814ed035cf4c2cb50ea72a76177fb1
syafiq-f <syafiq.fqh@gmail.com>
2025-09-08 15:59:56 +0700

    revisi allowed host

--- football_shop/settings.py
+++ football_shop/settings.py
@@ -32,7 +32,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "syafiq-faqih-footballnews.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "syafiq-faqih-footballshop.pbp.cs.ui.ac.id"]
 
 
 
